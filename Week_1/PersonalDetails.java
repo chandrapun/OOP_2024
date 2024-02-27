dbab0b8d6a000223a8270651772d1edac2d21572
Chandra Pun <chandrapun543@gmail.com>
2024-02-27 13:07:37 +0545

    PersonalDetails.java

---
+++ Week_1/PersonalDetails.java
@@ -0,0 +1,10 @@
+
+public class PersonalDetails {
+    public static void main(String[] args) {
+        System.out.println("\nPersonal Details:");
+        System.out.println("Name: " + "Chandra");
+        System.out.println("Address: " + "Kathmandu");
+        System.out.println("Age: " + "21");
+        System.out.println("Phone Number: " + "9821370051");
+    }
+}
