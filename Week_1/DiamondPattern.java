730fa104949d836e3e64dd5e7b5932505c8444b6
Chandra Pun <chandrapun543@gmail.com>
2024-02-27 13:25:06 +0545

    DiamondPattern

--- Week_1/DiamondPattern.java
+++ Week_1/DiamondPattern.java
@@ -1,32 +1,12 @@
 public class DiamondPattern {
     public static void main(String[] args) {
-        int n = 4; // Half of the diamond height (excluding the middle row)
-        
-        // Upper half
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k < 2 * i + 1; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        
-        // Lower half
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j < n - i; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k < 2 * i + 1; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
+	System.out.println("  *  ");
+	System.out.println(" *** ");
+	System.out.println("*****");
+	System.out.println(" *** ");
+	System.out.println("  *  ");
+	}
 }
-
-
 /* 
 
 a. What is the latest version of the Java SDK that is available?
